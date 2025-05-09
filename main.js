2ecf16ea7568f6f4da065899b79f728d321d0856
TBejarano24 <bejaranotomas04@gmail.com>
2025-05-09 01:40:46 -0300

    Commit inicial de la práctica calificada 10. El javascript obtiene y carga el contenido de forma dinámica y exitosa; a su vez, el sistema para detectar errores en la solicitud HTTP es perfectamente funcional, solamente hace falta perfeccionarlo a nivel estético. El estilo, creado a partir de tailwind, es completamente responsivo.

---
+++ main.js
@@ -0,0 +1,76 @@
+async function fetchUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    if (!response.ok) {
+      throw Error();
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+async function insertCard(usersData) {
+  const data = await usersData;
+  if (data !== null) {
+    data.forEach((element) => {
+      let div = document.createElement("div");
+      div.classList.add(
+        "m-3",
+        "mt-8",
+        "flex",
+        "flex-col",
+        "gap-1",
+        "p-3",
+        "shadow-[5px_7px_#00000058]",
+        "bg-white",
+        "rounded-xl",
+        "min-w-[290px]",
+        "justify-self-center"
+      );
+      let h3 = document.createElement("h3");
+      h3.innerHTML = `<strong>Nombre</strong>: ${element.name}`;
+      let p1 = document.createElement("p");
+      p1.innerHTML = `<strong>Usuario</strong>: ${element.username}`;
+      let p2 = document.createElement("p");
+      p2.innerHTML = `<strong>Email</strong>: ${element.email}`;
+      let p3 = document.createElement("p");
+      p3.innerHTML = `<strong>Empresa</strong>: ${element.company.name}`;
+      div.appendChild(h3);
+      div.appendChild(p1);
+      div.appendChild(p2);
+      div.appendChild(p3);
+      cardContainer.appendChild(div);
+    });
+  } else {
+    /*Acceder a la lista de clases de cardContainer y modificarlo para que me permita mostrar un sólo elemento centrado*/
+    let div = document.createElement("div");
+    div.classList.add(
+      "m-3",
+      "mt-8",
+      "flex",
+      "flex-col",
+      "gap-1",
+      "p-3",
+      "shadow-[5px_7px_#00000058]",
+      "bg-white",
+      "rounded-xl",
+      "min-w-[290px]",
+      "justify-self-center"
+    );
+    div.textContent = "ERROR";
+    cardContainer.appendChild(div);
+  }
+}
+
+let cardContainer = document.querySelector("#cardContainer");
+let regenerateButton = document.querySelector("#restartButton");
+
+regenerateButton.addEventListener("click", () => {
+  cardContainer.textContent = "";
+  insertCard(fetchUsers());
+});
+
+insertCard(fetchUsers());
